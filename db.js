9c2c0648870479b9b775172dd813b676dfcff21e
Memi20 <86634447+Memi20@users.noreply.github.com>
2026-06-23 23:52:14 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,15 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
+const Database = require('better-sqlite3');
+const path = require('path');
+const fs = require('fs');
+
+const dataDir = path.join(__dirname, 'data');
+if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
+
+const db = new Database(path.join(dataDir, 'floranya.db'));
+
 const db = new Database(path.join(__dirname, 'data', 'floranya.db'));
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
